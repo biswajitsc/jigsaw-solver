7f7c97245cc43823b5478ffe3352a45af40adf48
Biswajit Paria <biswajitsc@gmail.com>
2014-06-23 16:55:55 +0530

    Made src folder

--- compile_GeneratePieces.sh
+++ compile_GeneratePieces.sh
@@ -1,1 +1,1 @@
-g++ -ggdb  `pkg-config --cflags opencv` -o `basename generate_pieces.cpp .cpp` generate_pieces.cpp `pkg-config --libs opencv`
+g++ -ggdb  `pkg-config --cflags opencv` -o `basename generate_pieces.cpp .cpp` ./src/generate_pieces.cpp `pkg-config --libs opencv`
--- compile_solver.sh
+++ compile_solver.sh
@@ -1,1 +1,1 @@
-g++ -ggdb  `pkg-config --cflags opencv` -o `basename solver.cpp .cpp` solver.cpp `pkg-config --libs opencv`
+g++ -ggdb  `pkg-config --cflags opencv` -o `basename solver.cpp .cpp` ./src/solver.cpp `pkg-config --libs opencv`
